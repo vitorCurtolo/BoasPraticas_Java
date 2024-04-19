21b5d81b60c342dff8bf24727c7cb4f62e9654a0
Vitor Curtolo <vitor.curtolo09@gmail.com>
2024-04-19 16:49:09 -0300

    first commit

---
+++ src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -0,0 +1,39 @@
+package br.com.alura;
+
+import java.util.Scanner;
+
+public class AdopetConsoleApplication {
+
+    public static void main(String[] args) {
+        CommandExecutor executor = new CommandExecutor();
+        System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
+        try {
+            int opcaoEscolhida = 0;
+            while (true) {
+                exibirMenu();
+                String textoDigitado = new Scanner(System.in).nextLine();
+                opcaoEscolhida = Integer.parseInt(textoDigitado);
+
+                switch (opcaoEscolhida) {
+                    case 1 -> executor.executeCommand(new ListarAbrigosCommand());
+                    case 2 -> executor.executeCommand(new CadastrarAbrigoCommand());
+                    case 3 -> executor.executeCommand(new ListarPetsDoAbrigoCommand());
+                    case 4 -> executor.executeCommand(new ImportarPetsDoAbrigoCommand());
+                    case 5 -> System.exit(0);
+                    default -> opcaoEscolhida = 0;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void exibirMenu() {
+        System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
+        System.out.println("1 -> Listar abrigos cadastrados");
+        System.out.println("2 -> Cadastrar novo abrigo");
+        System.out.println("3 -> Listar pets do abrigo");
+        System.out.println("4 -> Importar pets do abrigo");
+        System.out.println("5 -> Sair");
+    }
+}
